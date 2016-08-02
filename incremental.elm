93f3e196ab5eccf34e67235d9cf5e74afdd1bcc9
Marco Guaspari Worms <marcogworms@gmail.com>
2016-08-02 16:59:39 -0300

    Add files via upload

--- incremental.elm
+++ incremental.elm
@@ -20,7 +20,8 @@ type alias Monster =
 
 
 type alias Player =
-    { xp : Int
+    { level : Int
+    , xp : Int
     , gold : Int
     , clickPower : Int
     , idlePower : Int
@@ -32,7 +33,6 @@ type alias Player =
 type alias Model =
     { player : Player
     , monster : Monster
-    , level : Int
     }
 
 
@@ -47,13 +47,36 @@ type Msg
     | Tick Float
 
 
+getLevel : Int -> Int
+getLevel xp =
+    let
+        xpTable =
+            [ 0, 5, 15, 30, 50, 80, 140, 220, 320, 450, 600 ]
+    in
+        (List.filter (\x -> x <= xp) xpTable) |> List.length
+
+
 addReward : Player -> Monster -> Player
 addReward player monster =
-    { player
-        | xp = player.xp + monster.xpReward
-        , gold = player.gold + monster.goldReward
-        , rebornStonesToAdd = monster.rebornStonesReward
-    }
+    let
+        newXp =
+            player.xp + monster.xpReward
+
+        newGold =
+            player.gold + monster.goldReward
+
+        newRebornStonesToAdd =
+            monster.rebornStonesReward
+
+        newLevel =
+            getLevel player.xp
+    in
+        { player
+            | xp = newXp
+            , gold = newGold
+            , rebornStonesToAdd = newRebornStonesToAdd
+            , level = newLevel
+        }
 
 
 damageMonster : Model -> Int -> Model
@@ -66,24 +89,6 @@ damageMonster model damage =
             { monster | hp = monster.hp - damage }
     in
         { model | monster = damagedMonster }
-
-
-newMonster : Int -> Monster
-newMonster levelMultiplier =
-    { hp = 100 * levelMultiplier
-    , maxHp = 100 * levelMultiplier
-    , xpReward = 1 * levelMultiplier
-    , goldReward = 3 * levelMultiplier
-    , rebornStonesReward = 1 * levelMultiplier // 10
-    }
-
-
-getLevel xp =
-    let
-        xpTable =
-            [ 0, 5, 15, 30, 50, 80, 140, 220, 320, 450, 600 ]
-    in
-        (List.filter (\x -> x <= xp) xpTable) |> List.length
 
 
 checkDeadMonster : Model -> Model
@@ -101,7 +106,6 @@ checkDeadMonster model =
         if monsterIsDead then
             { model
                 | player = newPlayer
-                , level = getLevel newPlayer.xp
                 , monster = newMonster (getLevel newPlayer.xp)
             }
         else
@@ -147,26 +151,43 @@ update msg model =
 -- VIEW
 
 
+makeHpBar : Int -> Int -> String -> String
+makeHpBar hp maxHp char =
+    List.foldr (++) char (List.repeat ((((hp * 100) // maxHp) // 10)) char)
+
+
 view : Model -> Html Msg
 view model =
-    div []
-        [ br [] []
-        , p [] [ text <| "Monster Hp: " ]
-        , p [] [ text <| List.foldr (++) "" (List.repeat ((((model.monster.hp * 100) // model.monster.maxHp) // 10) + 1) "|") ]
-        , p [] [ text <| toString model.monster.hp ++ " / " ++ toString model.monster.maxHp ]
-        , br [] []
-        , p [] [ text <| "Your Stats:" ]
-        , p [] [ text <| "Level: " ++ (toString model.level) ]
-        , p [] [ text <| "- Xp: " ++ (toString model.player.xp) ]
-        , p [] [ text <| "- Gold: " ++ (toString model.player.gold) ]
-          --, p [] [ text <| "- Click Power: " ++ (toString model.player.clickPower) ]
-        , p [] [ text <| "- Idle Power: " ++ (toString model.player.idlePower) ]
-        , br [] []
-        , p [] [ text <| "- Reborn Stones: " ++ (toString model.player.rebornStones) ]
-        , p [] [ text <| "Reborn Stones next reborn: +" ++ (toString model.player.rebornStonesToAdd) ]
-          --, button [ onClick HitMonster ] [ text "Hit!" ]
-        , button [ onClick Reborn ] [ text "Reborn" ]
-        ]
+    let
+        makeMonsterHpBar =
+            makeHpBar model.monster.hp model.monster.maxHp "|"
+
+        mainDivStyle =
+            style
+                [ ( "display", "block" )
+                , ( "position", "absolute" )
+                , ( "top", "10px" )
+                , ( "left", "20px" )
+                ]
+    in
+        div [ mainDivStyle ]
+            [ br [] []
+            , p [] [ text <| "Monster Hp: " ]
+            , p [] [ text <| makeMonsterHpBar ]
+            , p [] [ text <| toString model.monster.hp ++ " / " ++ toString model.monster.maxHp ]
+            , br [] []
+            , p [] [ text <| "Your Stats:" ]
+            , p [] [ text <| "- Level: " ++ (toString model.player.level) ]
+            , p [] [ text <| "- Xp: " ++ (toString model.player.xp) ]
+            , p [] [ text <| "- Gold: " ++ (toString model.player.gold) ]
+              --, p [] [ text <| "- Click Power: " ++ (toString model.player.clickPower) ]
+            , p [] [ text <| "- Idle Power: " ++ (toString model.player.idlePower) ]
+            , br [] []
+            , p [] [ text <| "- Reborn Stones: " ++ (toString model.player.rebornStones) ]
+            , p [] [ text <| "Reborn Stones next reborn: +" ++ (toString model.player.rebornStonesToAdd) ]
+              --, button [ onClick HitMonster ] [ text "Hit!" ]
+            , button [ onClick Reborn ] [ text "Reborn" ]
+            ]
 
 
 
@@ -184,11 +205,22 @@ subscriptions model =
 -- INIT
 
 
+newMonster : Int -> Monster
+newMonster levelMultiplier =
+    { hp = 100 * levelMultiplier
+    , maxHp = 100 * levelMultiplier
+    , xpReward = 1 * levelMultiplier
+    , goldReward = 3 * levelMultiplier
+    , rebornStonesReward = 1 * levelMultiplier // 10
+    }
+
+
 initialPlayerModel : Model
 initialPlayerModel =
     let
         player =
-            { xp = 0
+            { level = 1
+            , xp = 0
             , gold = 0
             , clickPower = 1
             , idlePower = 1
@@ -199,7 +231,7 @@ initialPlayerModel =
         monster =
             newMonster 1
     in
-        Model player monster 1
+        Model player monster
 
 
 init : ( Model, Cmd Msg )
